de914938966fc38aa9f83e0c828e28b3fcde21f2
sonnguyen0610 <17130194@st.hcmuaf.edu.vn>
2021-08-31 11:15:22 +0700

    + sau dau cham in hoa - chuyển toàn bộ ky tu dau thành in hoa

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,10 @@ public class App {
     public static void main(String[] args) {
         String str="You Only Live Once. But if You do it right. once is Enough";
         Cau1 upper = new Cau1();
-        System.out.println(upper.printUppercaseString(str));
+        System.out.println(str);
+//        System.out.println(upper.printUppercaseString(str));
         System.out.println(upper.countUpperString(str));
-//        System.out.println(upper.formatString(str));
+        System.out.println(upper.upperFisrtChar(str));
+        System.out.println(upper.formatString(str));
     }
 }
--- src/main/java/Cau1.java
+++ src/main/java/Cau1.java
@@ -1,32 +1,44 @@
 public class Cau1 {
-    
-    public String printUppercaseString(String s){
 
-        String res = "";
+//    public String printUppercaseString(String s) {
+//
+//        String res = "";
+//        for (int i = 0; i < s.length(); i++) {
+//            res = res + Character.toUpperCase(s.charAt(i));
+//        }
+//        return res;
+//    }
+
+    public int countUpperString(String s) {
+        int count = 0;
         for (int i = 0; i < s.length(); i++) {
-            res=res+Character.toUpperCase(s.charAt(i));
-        }
-        return res;
-    }
-    public int countUpperString(String s){
-        int count=0;
-        for (int i = 0; i < s.length(); i++) {
-            if(Character.isUpperCase(s.charAt(i))){
-              count++;
+            if (Character.isUpperCase(s.charAt(i))) {
+                count++;
             }
         }
         return count;
     }
-//    public String formatString(String s){
-//        String res="";
-//        String cau="";
-//        String [] tachCau=s.split(".");
-//        for (int i = 0; i < tachCau.length; i++) {
-//            for (int j = 0; j < tachCau[i].length(); j++) {
-//                cau+=Character.toUpperCase(tachCau[i].charAt(0));
-//            }
-//            res+=cau;
-//        }
-//        return res;
-//    }
+
+    public String upperFisrtChar(String s) {
+        String[] str = s.split("\\s+");
+        String res = "";
+        for (int i = 0; i < str.length; i++) {
+            res += str[i].substring(0, 1).toUpperCase();
+            res += str[i].substring(1) + " ";
+        }
+        return res;
+    }
+
+    public String formatString(String s) {
+        char[] chars = s.toCharArray();
+        String res = "";
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '.') {
+                chars[i + 2] = Character.toUpperCase(chars[i + 2]);
+            }
+            res += chars[i];
+        }
+        return res;
+    }
 }
